02e3d40b607a0a7ce9f9a243134359884cb82bd7
vanessamuller <vane9090@gmail.com>
2017-03-02 21:07:12 -0500

    solved sum all numbers in a range

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//
+// Sum All Numbers in a Range
+
+function sumAll(arr) {
+  var max =Math.max.apply(null, arr);
+  var min = Math.min.apply(null,arr);
+  var nums = [];
+  for(var i=min; i<=max; i++){
+    nums.push(i);
+  }
+
+  var sum;
+  nums.reduce(
+    function (
+      accumulator,
+      currentValue,
+      currentIndex,
+      array
+    ) {
+      sum = accumulator + currentValue;
+      return sum;
+    }
+  );
+  return sum;
+}
+
+
+sumAll([1, 4]);
