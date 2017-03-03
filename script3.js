4b98f322870b01c4b2200592f9ee3a027f33e005
vanessamuller <vane9090@gmail.com>
2017-03-03 17:43:11 -0500

    finished algorithm

--- script3.js
+++ script3.js
@@ -1,16 +1,20 @@
+
 function convertToRoman(num) {
+
+ var romanized = "";
 
  var romanNumeral = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
  var numbers = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
 
- for(i=0; i<numbers.length; i++){
-    while(num>numbers[i]){
+
+  for(i=0; i<numbers.length; i++){
+    while(num>=numbers[i]){
       romanized += romanNumeral[i];
       num -= numbers[i];
     }
   }
-  
- return num;
+
+ return romanized;
 }
 
 convertToRoman(36);
