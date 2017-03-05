baecf5cd51ab44c5457a813102944c9ba13ee350
vanessamuller <vane9090@gmail.com>
2017-03-04 21:04:52 -0500

    added comments to script 1

--- script1.js
+++ script1.js
@@ -1,14 +1,18 @@
 // Sum All Numbers in a Range
 
 function sumAll(arr) {
+  // Find the largest value in the array
   var max =Math.max.apply(null, arr);
+  // Find the smallest number in the array
   var min = Math.min.apply(null,arr);
   var nums = [];
+  // create an array with all the numbers from the minimum to the max
   for(var i=min; i<=max; i++){
     nums.push(i);
   }
 
   var sum;
+  // sum all numbers in the array and add them to the sum variable
   nums.reduce(
     function (
       accumulator,
