1beef8967f2f56c6b0a8802c0d0d1267e7cef6e5
vanessamuller <vane9090@gmail.com>
2017-03-04 20:56:39 -0500

    add comments to script3.js

--- script3.js
+++ script3.js
@@ -6,10 +6,13 @@ function convertToRoman(num) {
  var romanNumeral = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
  var numbers = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
 
-
+// go through the numbers array
   for(i=0; i<numbers.length; i++){
+    // if our number is greater than the greatest number is the numbers array
     while(num>=numbers[i]){
+      // add the equivalent romanNumeral of the greatest number of the numbers array to our romanized array
       romanized += romanNumeral[i];
+      // subtract the greatest number of our numbers array from our number and go thorugh the loop again
       num -= numbers[i];
     }
   }
