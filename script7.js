7848cd2a283b836773b876d6e4f4ddb54db42987
vanessamuller <vane9090@gmail.com>
2017-03-09 20:15:59 -0500

    added switch statement

--- script7.js
+++ script7.js
@@ -1,5 +1,21 @@
 // DNA Pairing
 function pairElement(str) {
+  var arr = str.split("");
+
+switch(pair){
+  case "C":
+    arr.push(["C", "G"]);
+    break;
+  case "G":
+    arr.push(["G","C"]);
+    break;
+  case "A":
+    arr.push(["A", "T"]);
+    break;
+  case "T":
+    arr.push(["T","A"]);
+    break;
+}
   return str;
 }
 
