f2ceeee56d1fc9c2e600dec8e0ecc97a345fe387
vanessamuller <vane9090@gmail.com>
2017-04-15 17:21:33 -0400

    solved boo who algorithm

---
+++ script9.js
@@ -0,0 +1,17 @@
+Boo Who
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  // check if the type of bool is a boolean
+  if(typeof bool === 'boolean'){
+    // if it is, return true
+    return true;
+  }
+  // if is not, return false
+  else{
+    return false;
+  }
+
+
+}
+
+booWho(null);
