e7ef8278fb41be1ec9588ddb674e4021d682b7f5
vanessamuller <vane9090@gmail.com>
2017-04-16 00:06:24 -0400

    added comments to script

--- script13.js
+++ script13.js
@@ -1,15 +1,21 @@
 
 function sumFibs(num) {
+  // start setting the first number
   var previousNum = 0;
+  // now set thr second number
   var currentNum = 1;
+  // the total is the first number until you start adding currentNum
   var total =0;
-
+  // while the current num os smaller or equal to num, we keep the addition
   while(currentNum<=num){
+    // if the current number is odd
     if(currentNum%2 !==0){
+      // add the current number to the total
       total +=currentNum;
     }
-
+    // the newc urrent number is the sum of the current number plus the previous one
     currentNum = currentNum+previousNum;
+    // the new previous number is the new current number minus the old previous number
     previousNum = currentNum - previousNum;
   }
 
